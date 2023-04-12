d7544dc6ac80d54b43b52b4986b69ebc8897b76e
Sabari H <64523847+sabari245@users.noreply.github.com>
2023-04-12 09:02:30 +0000

    template

---
+++ src/App.tsx
@@ -0,0 +1,155 @@
+function App() {
+
+  return (
+    <div className="App">
+      <header className="text-gray-600 body-font">
+        <div className="container flex flex-col flex-wrap items-center p-5 mx-auto md:flex-row">
+          <a className="flex items-center mb-4 font-medium text-gray-900 title-font md:mb-0">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} className="w-10 h-10 p-2 text-white bg-indigo-500 rounded-full" viewBox="0 0 24 24">
+              <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5" />
+            </svg>
+            <span className="ml-3 text-xl">Tailblocks</span>
+          </a>
+          <nav className="flex flex-wrap items-center justify-center text-base md:ml-auto md:mr-auto">
+            <a className="mr-5 hover:text-gray-900">First Link</a>
+            <a className="mr-5 hover:text-gray-900">Second Link</a>
+            <a className="mr-5 hover:text-gray-900">Third Link</a>
+            <a className="mr-5 hover:text-gray-900">Fourth Link</a>
+          </nav>
+          <button className="inline-flex items-center px-3 py-1 mt-4 text-base bg-gray-100 border-0 rounded focus:outline-none hover:bg-gray-200 md:mt-0">Button
+            <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} className="w-4 h-4 ml-1" viewBox="0 0 24 24">
+              <path d="M5 12h14M12 5l7 7-7 7" />
+            </svg>
+          </button>
+        </div>
+      </header>
+      <section className="text-gray-600 body-font">
+        <div className="container px-5 py-24 mx-auto">
+          <div className="flex flex-wrap -m-4">
+            <div className="w-full p-4 lg:w-1/4 md:w-1/2">
+              <a className="relative block h-48 overflow-hidden rounded">
+                <img alt="ecommerce" className="block object-cover object-center w-full h-full" src="https://dummyimage.com/420x260" />
+              </a>
+              <div className="mt-4">
+                <h3 className="mb-1 text-xs tracking-widest text-gray-500 title-font">CATEGORY</h3>
+                <h2 className="text-lg font-medium text-gray-900 title-font">The Catalyzer</h2>
+                <p className="mt-1">$16.00</p>
+              </div>
+            </div>
+            <div className="w-full p-4 lg:w-1/4 md:w-1/2">
+              <a className="relative block h-48 overflow-hidden rounded">
+                <img alt="ecommerce" className="block object-cover object-center w-full h-full" src="https://dummyimage.com/421x261" />
+              </a>
+              <div className="mt-4">
+                <h3 className="mb-1 text-xs tracking-widest text-gray-500 title-font">CATEGORY</h3>
+                <h2 className="text-lg font-medium text-gray-900 title-font">Shooting Stars</h2>
+                <p className="mt-1">$21.15</p>
+              </div>
+            </div>
+            <div className="w-full p-4 lg:w-1/4 md:w-1/2">
+              <a className="relative block h-48 overflow-hidden rounded">
+                <img alt="ecommerce" className="block object-cover object-center w-full h-full" src="https://dummyimage.com/422x262" />
+              </a>
+              <div className="mt-4">
+                <h3 className="mb-1 text-xs tracking-widest text-gray-500 title-font">CATEGORY</h3>
+                <h2 className="text-lg font-medium text-gray-900 title-font">Neptune</h2>
+                <p className="mt-1">$12.00</p>
+              </div>
+            </div>
+            <div className="w-full p-4 lg:w-1/4 md:w-1/2">
+              <a className="relative block h-48 overflow-hidden rounded">
+                <img alt="ecommerce" className="block object-cover object-center w-full h-full" src="https://dummyimage.com/423x263" />
+              </a>
+              <div className="mt-4">
+                <h3 className="mb-1 text-xs tracking-widest text-gray-500 title-font">CATEGORY</h3>
+                <h2 className="text-lg font-medium text-gray-900 title-font">The 400 Blows</h2>
+                <p className="mt-1">$18.40</p>
+              </div>
+            </div>
+            <div className="w-full p-4 lg:w-1/4 md:w-1/2">
+              <a className="relative block h-48 overflow-hidden rounded">
+                <img alt="ecommerce" className="block object-cover object-center w-full h-full" src="https://dummyimage.com/424x264" />
+              </a>
+              <div className="mt-4">
+                <h3 className="mb-1 text-xs tracking-widest text-gray-500 title-font">CATEGORY</h3>
+                <h2 className="text-lg font-medium text-gray-900 title-font">The Catalyzer</h2>
+                <p className="mt-1">$16.00</p>
+              </div>
+            </div>
+            <div className="w-full p-4 lg:w-1/4 md:w-1/2">
+              <a className="relative block h-48 overflow-hidden rounded">
+                <img alt="ecommerce" className="block object-cover object-center w-full h-full" src="https://dummyimage.com/425x265" />
+              </a>
+              <div className="mt-4">
+                <h3 className="mb-1 text-xs tracking-widest text-gray-500 title-font">CATEGORY</h3>
+                <h2 className="text-lg font-medium text-gray-900 title-font">Shooting Stars</h2>
+                <p className="mt-1">$21.15</p>
+              </div>
+            </div>
+            <div className="w-full p-4 lg:w-1/4 md:w-1/2">
+              <a className="relative block h-48 overflow-hidden rounded">
+                <img alt="ecommerce" className="block object-cover object-center w-full h-full" src="https://dummyimage.com/427x267" />
+              </a>
+              <div className="mt-4">
+                <h3 className="mb-1 text-xs tracking-widest text-gray-500 title-font">CATEGORY</h3>
+                <h2 className="text-lg font-medium text-gray-900 title-font">Neptune</h2>
+                <p className="mt-1">$12.00</p>
+              </div>
+            </div>
+            <div className="w-full p-4 lg:w-1/4 md:w-1/2">
+              <a className="relative block h-48 overflow-hidden rounded">
+                <img alt="ecommerce" className="block object-cover object-center w-full h-full" src="https://dummyimage.com/428x268" />
+              </a>
+              <div className="mt-4">
+                <h3 className="mb-1 text-xs tracking-widest text-gray-500 title-font">CATEGORY</h3>
+                <h2 className="text-lg font-medium text-gray-900 title-font">The 400 Blows</h2>
+                <p className="mt-1">$18.40</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <footer className="text-gray-600 body-font">
+        <div className="container flex flex-col items-center px-5 py-8 mx-auto sm:flex-row">
+          <a className="flex items-center justify-center font-medium text-gray-900 title-font md:justify-start">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} className="w-10 h-10 p-2 text-white bg-indigo-500 rounded-full" viewBox="0 0 24 24">
+              <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5" />
+            </svg>
+            <span className="ml-3 text-xl">Tailblocks</span>
+          </a>
+          <p className="mt-4 text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0">© 2020 Tailblocks —
+            <a href="https://twitter.com/knyttneve" className="ml-1 text-gray-600" rel="noopener noreferrer" target="_blank">@knyttneve</a>
+          </p>
+          <span className="inline-flex justify-center mt-4 sm:ml-auto sm:mt-0 sm:justify-start">
+            <a className="text-gray-500">
+              <svg fill="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} className="w-5 h-5" viewBox="0 0 24 24">
+                <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z" />
+              </svg>
+            </a>
+            <a className="ml-3 text-gray-500">
+              <svg fill="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} className="w-5 h-5" viewBox="0 0 24 24">
+                <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z" />
+              </svg>
+            </a>
+            <a className="ml-3 text-gray-500">
+              <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} className="w-5 h-5" viewBox="0 0 24 24">
+                <rect width={20} height={20} x={2} y={2} rx={5} ry={5} />
+                <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01" />
+              </svg>
+            </a>
+            <a className="ml-3 text-gray-500">
+              <svg fill="currentColor" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth={0} className="w-5 h-5" viewBox="0 0 24 24">
+                <path stroke="none" d="M16 8a6 6 0 016 6v7h-4v-7a2 2 0 00-2-2 2 2 0 00-2 2v7h-4v-7a6 6 0 016-6zM2 9h4v12H2z" />
+                <circle cx={4} cy={4} r={2} stroke="none" />
+              </svg>
+            </a>
+          </span>
+        </div>
+      </footer>
+
+    </div>
+  )
+}
+
+export default App
